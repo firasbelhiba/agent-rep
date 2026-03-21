406328ed1f773f07ef479434f5dca2adc5fc19ea
firasbelhiba <firas.belhiba@esprit.tn>
2026-03-21 23:22:23 +0100

    Fixing a blurr issue

--- start-backend.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d "%~dp0backend"
-npx ts-node -r tsconfig-paths/register src/main.ts
--- start-dev.js
+++
@@ -1,3 +0,0 @@
-const { execSync } = require("child_process");
-const port = process.env.PORT || 3000;
-execSync(`npx next dev -p ${port}`, { stdio: "inherit", shell: true });
